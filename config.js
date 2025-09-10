dfb6ac2685d29b31ad09955022546b2a6ecffb69
DominicDev <s0l0estudi4r@gmail.com>
2025-09-10 17:50:44 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FLOW';
+export const nameSongs = [
+	"HERO ", /*1*/
+	"GO", /*2*/
+	"COLORS", /*3*/
+	"SIGN", /*4*/
+	"DAYS ", /*5*/
+	"VOYAGE", /*6*/
+	"SEVEN", /*7*/
+	"HOWLING", /*8*/
+	"NIJI NO SOR", /*9*/
+	"KAZENO UTA", /*10*/
+	"BURN ", /*11*/
+	"STEPPIN’ OUT ", /*12*/
+	"BRAVELUE ", /*13*/
+	"INNOSENSE", /*14*/
+	"NEIRO", /*15*/
+	"ONENESS", /*16*/
+	"WORLD END", /*17*/
+	"DICE", /*18*/
+	"PENDULUM ", /*19*/
+	"LOST ", /*20*/
+];
